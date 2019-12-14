2e78cbe1ca2377a563b6bdbee44c379f502a72c7
eliahanna <eli.a.hanna@gmail.com>
2019-12-14 12:24:27 -0800

    function for getting current weather

---
+++ office_temp.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+def getWeather():
+    weather_api = 'ca21216fd309c93287df5ad643170ed1'
+    response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Seattle&appid="+weather_api+"&units=imperial")
+    # parse response
+    response_str = ""
+    for line in response:
+        response_str += line.decode("utf-8") 
+        # parse json
+    response_json = json.loads(response_str)
+    current_temp = response_json["main"]["temp"]
+    min_temp = response_json["main"]["temp_min"]
+    max_temp = response_json["main"]["temp_max"]
+    return str(current_temp), str(min_temp), str(max_temp)
+
+current_temp, min_temp, max_temp = getWeather()
+
+print("Current temp: "+current_temp + " Min temp: " +  min_temp +  " Max temp: "  + max_temp)
